d18f7e4daf045a2de6c20812a624cce3a2dbca5f
Kaban17 <kaban1628@gmail.com>
2025-07-22 19:08:02 +0400

    add task L1/20

--- L1/20/main.go
+++ L1/20/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func reverseRunes(runes []rune, start, end int) {
+	for start < end {
+		runes[start], runes[end] = runes[end], runes[start]
+		start++
+		end--
+	}
+}
+
+func reverseWordsInPlace(s string) string {
+	runes := []rune(s)
+
+	reverseRunes(runes, 0, len(runes)-1)
+
+	start := 0
+	for i := 0; i <= len(runes); i++ {
+		if i == len(runes) || unicode.IsSpace(runes[i]) {
+			reverseRunes(runes, start, i-1)
+			start = i + 1
+		}
+	}
+
+	return string(runes)
+}
 
 func main() {
-    fmt.Println("Hello, world from L1/20!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Введите строку: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	result := reverseWordsInPlace(input)
+	fmt.Println("Перевёрнутая строка:", result)
 }
