504b01042dbf86b3a7bf51d3722bf6ee1761c628
Kaban17 <kaban1628@gmail.com>
2025-07-22 18:52:29 +0400

    add task L1/18

--- L1/18/main.go
+++ L1/18/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
+var cnt int
+var mu sync.Mutex
+var wg sync.WaitGroup
+
+func inc() {
+	mu.Lock()
+	defer mu.Unlock()
+	cnt++
+	wg.Done()
+}
 func main() {
-    fmt.Println("Hello, world from L1/18!")
+	fmt.Println("Hello, world from L1/18!")
+	for range 100 {
+		wg.Add(1)
+		go inc()
+	}
+	wg.Wait()
+	fmt.Println(cnt)
 }
