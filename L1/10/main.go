6df8352aa3360f7be40c4bea3f127f1ad9f07bf7
Kaban17 <kaban1628@gmail.com>
2025-07-22 13:46:45 +0400

    add task L1/10

--- L1/10/main.go
+++ L1/10/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-    fmt.Println("Hello, world from L1/10!")
+	temps := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	groups := make(map[int][]float64)
+	for _, temp := range temps {
+		key := int(math.Floor(temp/10.0)) * 10
+		groups[key] = append(groups[key], temp)
+	}
+	for key := range groups {
+		fmt.Printf("Group %d: %v\n", key, groups[key])
+	}
+
 }
