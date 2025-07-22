2f6465445fd6c3e51002ebeb637550008aa2a181
Kaban17 <kaban1628@gmail.com>
2025-07-22 18:32:18 +0400

    add task L1/14

--- L1/14/main.go
+++ L1/14/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func detectType(v any) {
+	switch val := v.(type) {
+	case int:
+		fmt.Printf("Тип: int, значение: %d\n", val)
+	case string:
+		fmt.Printf("Тип: string, значение: %q\n", val)
+	case bool:
+		fmt.Printf("Тип: bool, значение: %t\n", val)
+	case chan int:
+		fmt.Println("Тип: chan int")
+	case chan string:
+		fmt.Println("Тип: chan string")
+	case chan bool:
+		fmt.Println("Тип: chan bool")
+	default:
+		fmt.Printf("Неизвестный тип: %T\n", val)
+	}
+}
 
 func main() {
-    fmt.Println("Hello, world from L1/14!")
+	detectType(42)
+	detectType("hello")
+	detectType(true)
+	detectType(make(chan int))
+	detectType(make(chan string))
+	detectType(make(chan bool))
+	detectType(3.14)
 }
