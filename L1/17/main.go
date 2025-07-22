e271de287ba971a2ba9d802831d4e7469b8bcbcd
Kaban17 <kaban1628@gmail.com>
2025-07-22 18:46:10 +0400

    add task L1/17

--- L1/17/main.go
+++ L1/17/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+func binarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
 func main() {
-    fmt.Println("Hello, world from L1/17!")
+	arr := []int{1, 3, 5, 7, 9, 11, 15}
+	fmt.Println("Index 7:", binarySearch(arr, 7))   // 3
+	fmt.Println("Index 4:", binarySearch(arr, 4))   // -1
+	fmt.Println("Index 1:", binarySearch(arr, 1))   // 0
+	fmt.Println("Index 15:", binarySearch(arr, 15)) // 6
 }
