79d06553d3f5507deb7ee4ee5e7f42e481cc3694
Kaban17 <kaban1628@gmail.com>
2025-07-22 18:42:28 +0400

    add task L1/16

--- L1/16/main.go
+++ L1/16/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func quickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	pivot := arr[0]
+	less := []int{}
+	greater := []int{}
+
+	for _, val := range arr[1:] {
+		if val <= pivot {
+			less = append(less, val)
+		} else {
+			greater = append(greater, val)
+		}
+	}
+
+	return append(append(quickSort(less), pivot), quickSort(greater)...)
+}
 
 func main() {
-    fmt.Println("Hello, world from L1/16!")
+	arr := []int{10, 5, 2, 3, 8, 9, 1, 4}
+	sorted := quickSort(arr)
+	fmt.Println("Отсортированный массив:", sorted)
 }
