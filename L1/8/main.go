5cdc7d4c5cf0c9758df731020ffa137a1849f27a
Kaban17 <kaban1628@gmail.com>
2025-07-22 11:32:43 +0400

    add task L1/8

--- L1/8/main.go
+++ L1/8/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func setBit(n int64, i uint) int64 {
+	return n | (1 << i)
+}
+func clearBit(n int64, i uint) int64 {
+	return n &^ (1 << i)
+}
 func main() {
-    fmt.Println("Hello, world from L1/8!")
+	var num int64 = 10
+	fmt.Printf("Decimal: %d. Binary: %b\n", num, num)
+	var bitPos uint = 1
+	fmt.Printf("Decimal: %d. Binary: %b\n", clearBit(num, bitPos), clearBit(num, bitPos))
+	fmt.Printf("Decimal: %d. Binary: %b\n", setBit(num, bitPos), setBit(num, bitPos))
 }
