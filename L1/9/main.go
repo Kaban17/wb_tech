13b8a0cd2b424cc5d14d843594afa71e140967e1
Kaban17 <kaban1628@gmail.com>
2025-07-22 12:19:27 +0400

    add task L1/9

--- L1/9/main.go
+++ L1/9/main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+func double(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * 2
+		}
+		close(out)
+	}()
+	return out
+}
+func print(in <-chan int) {
+	for n := range in {
+		fmt.Println(n)
+	}
+}
 func main() {
-    fmt.Println("Hello, world from L1/9!")
+	c1 := gen(1, 2, 3, 4, 5, 6)
+	c2 := double(c1)
+	print(c2)
 }
