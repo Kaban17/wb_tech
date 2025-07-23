e2fbe6e0042679b503b7cba2859955a718e2cfd6
Kaban17 <kaban1628@gmail.com>
2025-07-23 23:03:10 +0400

    add task L1/25

--- L1/25/main.go
+++ L1/25/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+func sleep(d time.Duration) {
+	start := time.Now()
+	for time.Since(start) < d {
+		continue
+
+	}
+}
 func main() {
-    fmt.Println("Hello, world from L1/25!")
+	fmt.Println("Hello, world from L1/25!")
+	fmt.Println(time.Now())
+	time.Sleep(2 * time.Second)
+	fmt.Println(time.Now())
 }
