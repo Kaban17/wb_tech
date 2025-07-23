866cd74a933bb894e0d0b7eb226cece879479ad2
Kaban17 <kaban1628@gmail.com>
2025-07-23 22:44:03 +0400

    add task L1/23

--- L1/23/main.go
+++ L1/23/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, world from L1/23!")
+	data := []int{1, 2, 3, 4, 5}
+	fmt.Println(data)
+	fmt.Println(data[4])
+	data = remove(data, 2) // Remove the element at index 2
+	fmt.Println(data)
+	// fmt.Println(data[4])  panic: runtime error: index out of range [4] with length 4
+}
+func remove(slice []int, index int) []int {
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
 }
