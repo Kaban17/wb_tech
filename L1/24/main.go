8a5a491f8dfff8848058b0f907a8809816743cb7
Kaban17 <kaban1628@gmail.com>
2025-07-23 22:46:42 +0400

    add task L1/24

--- L1/24/main.go
+++ L1/24/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+type Point2D struct {
+	x, y float64
+}
+
+func NewPoint2D(x, y float64) Point2D {
+	return Point2D{x, y}
+}
+func (p Point2D) Distance(other Point2D) float64 {
+	return math.Sqrt(math.Pow(other.x-p.x, 2) + math.Pow(other.y-p.y, 2))
+}
 func main() {
-    fmt.Println("Hello, world from L1/24!")
+	p := NewPoint2D(1, 2)
+	fmt.Println(p.Distance(NewPoint2D(2, 1)))
 }
