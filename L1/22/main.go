2f98f4bc435cb04f0835b4a8b2209d5efffdb721
Kaban17 <kaban1628@gmail.com>
2025-07-23 22:24:40 +0400

    add task L1/21

--- L1/22/main.go
+++ L1/22/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func main() {
-    fmt.Println("Hello, world from L1/22!")
+	// Создаем большие числа (примеры значений > 2^20)
+	a := new(big.Int)
+	b := new(big.Int)
+
+	// Устанавливаем значения из строк (можно использовать числа любого размера)
+	a.SetString("1000000000000000000000", 10) // 10^21
+	b.SetString("500000000000000000000", 10)  // 5*10^20
+
+	// Выполняем операции
+	fmt.Println("Сложение:", add(a, b))
+	fmt.Println("Вычитание:", sub(a, b))
+	fmt.Println("Умножение:", mul(a, b))
+	fmt.Println("Деление:", div(a, b))
+}
+
+// Сложение больших чисел
+func add(a, b *big.Int) *big.Int {
+	return new(big.Int).Add(a, b)
+}
+
+// Вычитание больших чисел
+func sub(a, b *big.Int) *big.Int {
+	return new(big.Int).Sub(a, b)
+}
+
+// Умножение больших чисел
+func mul(a, b *big.Int) *big.Int {
+	return new(big.Int).Mul(a, b)
+}
+
+// Деление больших чисел (с обработкой деления на ноль)
+func div(a, b *big.Int) *big.Float {
+	if b.Sign() == 0 {
+		panic("Деление на ноль!")
+	}
+
+	// Конвертируем big.Int в big.Float для точного деления
+	fa := new(big.Float).SetInt(a)
+	fb := new(big.Float).SetInt(b)
+
+	return new(big.Float).Quo(fa, fb)
 }
