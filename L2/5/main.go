f9425cae946e17cc0dac3831a1d6fee84269c14e
Kaban17 <kaban1628@gmail.com>
2025-07-26 20:42:30 +0400

    add task L2/5

--- L2/5/main.go
+++ L2/5/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type customError struct {
+	msg string
+}
+
+func (e *customError) Error() string {
+	return e.msg
+}
+
+func test() *customError {
+	// ... do something
+	return nil
+}
+
 func main() {
-    fmt.Println("Hello, world from L2/5!")
+	var err error
+	err = test()
+	if err != nil {
+		println("error")
+		fmt.Printf("NOT nil: type = %T, value = %v\n", err, err)
+		return
+	}
+	println("ok")
 }
