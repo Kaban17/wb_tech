29bb2836fabaef7991bccf041a105bc856ffedfc
Kaban17 <kaban1628@gmail.com>
2025-08-08 19:50:41 +0400

    add task L2/11

--- L2/11/main.go
+++ L2/11/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"sort"
+	"strings"
+	"unicode"
+)
+
+func sortStringByRunes(s string) string {
+	runes := []rune(s)
+	slices.Sort(runes)
+	return string(runes)
+}
+
+func findAnagramSets(words []string) map[string][]string {
+	tempMap := make(map[string][]string)
+
+	for _, word := range words {
+		lowerWord := strings.ToLower(word)
+		if !isLetterString(lowerWord) {
+			continue
+		}
+
+		canonical := sortStringByRunes(lowerWord)
+
+		tempMap[canonical] = append(tempMap[canonical], lowerWord)
+	}
+
+	result := make(map[string][]string)
+
+	for _, anagrams := range tempMap {
+		if len(anagrams) < 2 {
+			continue
+		}
+
+		sort.Strings(anagrams)
+
+		key := anagrams[0]
+		result[key] = anagrams
+	}
+
+	return result
+}
+
+func isLetterString(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-    fmt.Println("Hello, world from L2/11!")
+	words := []string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик", "стол"}
+	anagrams := findAnagramSets(words)
+
+	fmt.Println("Входные слова:", words)
+	fmt.Println("Найденные множества анаграмм:")
+	for key, value := range anagrams {
+		fmt.Printf("- %q: %v\n", key, value)
+	}
 }
