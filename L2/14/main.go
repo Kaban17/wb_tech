df761e43c95549feb71296d7d8de9307d5acffe2
Kaban17 <kaban1628@gmail.com>
2025-08-14 17:01:59 +0400

    add task L2/14

--- L2/14/main.go
+++ L2/14/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// or реализует функцию, которая объединяет несколько каналов.
+// Возвращаемый канал закрывается, как только закрывается любой
+// из исходных каналов.
+func or(channels ...<-chan any) <-chan any {
+	switch len(channels) {
+	case 0:
+		c := make(chan any)
+		close(c)
+		return c
+	case 1:
+		return channels[0]
+	default:
+		m := len(channels) / 2
+		return mergeTwo(or(channels[:m]...), or(channels[m:]...))
+	}
+}
+
+func mergeTwo(a, b <-chan any) <-chan any {
+	c := make(chan any)
+	go func() {
+		defer close(c)
+		select {
+		case <-a:
+		case <-b:
+		}
+	}()
+	return c
+}
 
 func main() {
-    fmt.Println("Hello, world from L2/14!")
+	sig := func(after time.Duration) <-chan any {
+		c := make(chan any)
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("Готово после %v\n", time.Since(start))
 }
